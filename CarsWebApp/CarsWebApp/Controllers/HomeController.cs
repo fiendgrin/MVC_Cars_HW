eafdf3b01a7e210ed8b1c622862fc1d240afce23
fiendgrin <yunisah@code.edu.az>
2023-08-20 16:31:48 +0400

    All Folders added, HomeController added

---
+++ CarsWebApp/CarsWebApp/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarsWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
