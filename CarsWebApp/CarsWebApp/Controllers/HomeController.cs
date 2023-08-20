aea990b139ca27aa6d7c41bf8afa2e910d746109
fiendgrin <yunisah@code.edu.az>
2023-08-20 16:47:22 +0400

    Brand model added, List of brands done,Home-Index View done

--- CarsWebApp/CarsWebApp/Controllers/HomeController.cs
+++ CarsWebApp/CarsWebApp/Controllers/HomeController.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using CarsWebApp.Models;
 
 namespace CarsWebApp.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly List<Brand> _brands;
+
+        public HomeController()
+        {
+            _brands = new List<Brand>() {
+                new Brand{Id=1,Name="Porsche",Manufacturer="Germany",Foundation=1948},
+                new Brand{Id=2,Name="BMW",Manufacturer="Germany",Foundation=1916},
+                new Brand{Id=3,Name="Nissan",Manufacturer="Japan",Foundation=1933},
+            };
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(_brands);
         }
+
     }
 }
---
+++ CarsWebApp/CarsWebApp/Models/Brand.cs
@@ -0,0 +1,10 @@
+﻿namespace CarsWebApp.Models
+{
+    public class Brand
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public int Foundation { get; set; }
+    }
+}
